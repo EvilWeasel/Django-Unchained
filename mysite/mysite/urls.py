a25f5df5a12b31304b9b47e215ef6b918b161594
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-05-16 20:59:51 +0200

    added accounts app and demo for home and contact route in router

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def Home(request):
+    return HttpResponse("Home Page")
+def Contact(request):
+    return HttpResponse("Contact Page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', Home),
+    path('contact/', Contact),
 ]
