f24e48467053ddd537f0bba7e0fd604691814bb2
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2022-05-18 05:09:16 +0200

    added more models

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, Orders, Product
 
 # Register your models here.
 
 
 admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Orders)
--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 from django.db import models
 
 # Create your models here.
@@ -11,3 +13,26 @@ class Customer(models.Model):
 
   def __str__(self):
     return self.name
+
+class Product(models.Model):
+  CATEGORY = (
+    ('Indoor', 'Indoor'),
+    ('Out Door', 'Out Door'),
+  )
+  name = models.CharField(max_length=200, null=True)
+  price = models.FloatField(null=True)
+  category =models.CharField(max_length=200, null=True, choices=CATEGORY)
+  desciption = models.CharField(max_length=200, null=True)
+  date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+  STATUS = (
+    ('Pending', 'Pending'),
+    ('Out for delivery', 'Out for delivery'),
+    ('Delivered', 'Delivered'),
+  )
+
+  # customer = 
+  # product =
+  date_created = models.DateTimeField(auto_now_add=True, null=True)
+  status = models.CharField(max_length=200, null=True, choices=STATUS)
